11812249b1accc3d015d601cb898d9f3c85dac97
joynalabedin05 <joynal05101993@gmail.com>
2023-05-07 07:29:03 +0600

    m-66-simple-server added

---
+++ README.md
@@ -0,0 +1,1 @@
+# m-66-simple-crud-server
---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+// midleware
+// joynal05101993
+// GQo7LBadSRtutyMn
+
+
+
+const uri = "mongodb+srv://joynal05101993:GQo7LBadSRtutyMn@cluster0.ssvrn1a.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res)=>{
+    res.send('simple crud running');
+});
+
+app.listen(port, ()=>{
+    console.log(`simple crud is running on port: ${port}`);
+})
